a1e91257aef833011abb3fed0175a2962aa4d633
Gabriel Bassinello <gabrielbassinello@hotmail.com>
2021-05-16 20:01:21 -0300

    feat: removed plugged nvim folder

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ sudo pacman -Syyu curl git yay
 git clone https://gist.github.com/df977bbcd3685fc75bca52f1ccc8b398.git ~/gist
 mkdir ~/.config/Code
 mkdir ~/.config/Code/User
+mkdir ~/.config/nvim
+mkdir ~/.config/nvim/plugged
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
