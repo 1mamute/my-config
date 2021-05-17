0bb6acd336b31d4bcf5b8b6310966c0c4dc6b790
Gabriel Bassinello <gabrielbassinello@hotmail.com>
2021-05-16 23:58:55 -0300

    feat: asdf on install script

--- install.sh
+++ install.sh
@@ -11,3 +11,27 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 yay -S $(tr '\n' ' ' < ~/gist/manjaro.xfce.pacmanity) --cleanafter --answerclean a --answerdiff n --answeredit n --answerupgrade a
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+asdf update
+asdf plugin add direnv 
+asdf plugin add flux2
+asdf plugin add github-cli
+asdf plugin add golang
+asdf plugin add helm
+asdf plugin add nodejs
+asdf plugin add k9s
+asdf plugin add kubectl
+asdf plugin add packer
+asdf plugin add python
+asdf plugin add terraform
+asdf install direnv latest
+asdf install flux2 latest
+asdf install github-cli latest 
+asdf install golang latest
+asdf install helm latest
+asdf install nodejs lts
+asdf install k9s latest 
+asdf install kubectl latest
+asdf install packer latest
+asdf install python 3.9.5
+asdf install terraform latest
